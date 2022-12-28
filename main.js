32e20d4d381b85c5cdc4751213628cdf145b372d
abdelaziz <abdelaazizeelhathoute.2018@gmail.com>
2022-12-28 13:43:02 +0100

    add divider line between bran and ul in media query

---
+++ main.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+    $('#nav-toggle').click(function() {
+        $('nav ul').slideToggle();
+       
+      });
+      $('#nav-toggle').on('click', function() {
+        this.classList.toggle('active')
+      
+      });
+  
+    //navbar par defaut black
+      $(".navigation ").css('background-color','black');
+     $("ul>li>a").css('background-color','black');
+     $("ul>li>.search").css('background-color','black');
+     $("ul>li>a").css('color','white');
+      $("#svg-icon-brand").css('background-color','white');
+  //display none of icon mode black
+  $("#toggle-icon-mode-black").css('display','none');
+  //click in  icon white mode
+  $("#toggle-icon-mode-white").click(function(){
+     $("body").css("background-color","black");
+     $("#toggle-icon-mode-black").css('display', 'block');
+     $(this).css('display', 'none');
+  
+  //active white mode
+     $(".navigation ").css('background-color','white');
+     $("ul>li>a").css('background-color','white');
+     $("ul>li>.search").css('background-color','white');
+     $("ul>li>a").css('color','black');
+    $("#svg-icon-brand").css('background-color','white');
+  
+  });
+  //click in icon dark mode
+     $("#toggle-icon-mode-black").click(function(){
+     $("body").css("background-color","white");
+     $("#toggle-icon-mode-white").css('display', 'block');
+     $(this).css('display', 'none');
+   //return a dark mode
+     $(".navigation ").css('background-color','black');
+     $("ul>li>a").css('background-color','black');
+     $("ul>li>.search").css('background-color','black');
+     $("ul>li>a").css('color','white');
+      $("#svg-icon-brand").css('background-color','white');
+  });
+  
+
+});
